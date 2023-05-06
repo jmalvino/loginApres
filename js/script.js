b39bad5a41f9c7e55c4e6a39d252dc704aeea005
João Malvino Júnior <jmjr77@hotmail.com>
2023-05-05 21:38:30 -0300

    Login Apresentacao

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function mensagem_Sucesso() {
+    Swal.fire({
+        title: 'ATENÇÃO!',
+        text: 'Preencha os dados com e-mail válido',
+        icon: 'error',
+        confirmButtonColor: '#212529', // cor do botão
+    theme: 'sweetalert2-theme-dark', // tema escuro
+    });
+}
+
