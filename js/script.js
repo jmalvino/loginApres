dcc337501152359fe7c3a0694b383b98785b1e93
João Malvino Júnior <jmjr77@hotmail.com>
2023-05-16 08:40:17 -0300

    alteracoes no footer e selecao all dos checkbox da tela config

--- js/script.js
+++ js/script.js
@@ -9,3 +9,34 @@ function mensagem_Sucesso() {
     });
 }
 
+function verificarCheckboxes() {
+    var checkboxes = document.querySelectorAll('.telalogin input[type="checkbox"]');
+    var button = document.getElementById('teste');
+
+    var anyChecked = false;
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            anyChecked = true;
+            break;
+        }
+    }
+
+    button.disabled = !anyChecked;
+}
+
+function selecionarTodosCheckboxes() {
+    var checkboxes = document.querySelectorAll('.telalogin input[type="checkbox"]');
+    var button = document.getElementById('teste');
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = true;
+    }
+
+    button.disabled = true;
+}
+
+// Attach event listener to checkboxes
+var checkboxes = document.querySelectorAll('.telalogin input[type="checkbox"]');
+for (var i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener('click', verificarCheckboxes);
+}
